33992080602e6843d71064268e2edbd8ae1c3af1
Finlay <finjsawyer@gmail.com>
2020-10-21 10:55:16 +0100

    Add in web framework

--- src/main/java/training/metofficeweather/Main.java
+++ src/main/java/training/metofficeweather/Main.java
@@ -1,17 +1,6 @@
 package training.metofficeweather;
 
-import org.glassfish.jersey.jackson.JacksonFeature;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.MediaType;
-
 public class Main {
     public static void main(String[] args) {
-        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
-        String name = client.target("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/310042?res=3hourly&key=")
-                .request(MediaType.APPLICATION_JSON)
-                .get(String.class);
-        System.out.println(name);
     }
 }	
--- src/main/java/training/metofficeweather/web/WeatherInfo.java
+++ src/main/java/training/metofficeweather/web/WeatherInfo.java
@@ -1,5 +1,13 @@
 package training.metofficeweather.web;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.util.List;
+
 public class WeatherInfo {
     private final String locationId;
 
@@ -9,5 +17,15 @@ public class WeatherInfo {
 
     public String getLocationId() {
         return locationId;
+    }
+
+    public String getMetOfficeData(String locationId) throws IOException {
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+        String data = client.target("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/"
+                + locationId + "?res=3hourly&key=KEY")
+                .request(MediaType.APPLICATION_JSON)
+                .get(String.class);
+
+        return data;
     }
 }
